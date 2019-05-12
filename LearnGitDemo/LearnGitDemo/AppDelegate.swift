0b09b3dc7526440a0cd84337ffdfc0a300f5427c
xu-laoshi <jason.hsu@163.com>
2019-05-12 18:28:07 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LearnGitDemo
+//
+//  Created by 胥老师 on 2019/5/12.
+//  Copyright © 2019 胥老师. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LearnGitDemo/LearnGitDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearnGitDemo
+//
+//  Created by 胥老师 on 2019/5/12.
+//  Copyright © 2019 胥老师. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
